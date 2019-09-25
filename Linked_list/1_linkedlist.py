bed72509831a0d7bae8df510550b158f15eb0d53
Raghu <raghu.ndp@gmail.com>
2019-09-25 19:17:36 +0530

    Add files via upload

---
+++ Linked_list/1_linkedlist.py
@@ -0,0 +1,92 @@
+class Node():
+  def __init__(self, data):
+    self.data = data
+    self.nextNode = None
+    
+class LinkedList():
+  def __init__(self):
+    self.head = None
+    self.size = 0
+   
+  # O(1) 
+  # Insert at begining
+  def insertStart(self, data):
+    self.size = self.size + 1
+    newNode = Node(data)
+    if not self.head:
+      self.head = newNode
+    else:
+      newNode.nextNode = self.head
+      self.head = newNode
+      
+  
+  # Remove node
+  def remove(self, data):
+    if self.head is None:
+      return
+    self.size = self.size -1
+    
+    currentNode = self.head
+    previousNode = None
+      
+    while currentNode.data != data:
+      previousNode = currentNode
+      currentNode = currentNode.nextNode
+      
+    if previousNode is None:
+      self.head = currentNode.nextNode
+    else:
+      previousNode.nextNode = currentNode.nextNode
+      
+      
+    
+  # O(1)
+  def size1(self):
+    return self.size
+  
+  # O(N) --> not good
+  def size2(self):
+    actualNode = self.head
+    size=0
+    
+    while actualNode is not None:
+      size+=1
+      actualNode = actualNode.nextNode
+      
+    return size
+  
+  # Insert at end
+  # O(N)
+  def insertEnd(self, data):
+    self.size = self.size + 1
+    newNode = Node(data)
+    actualNode = self.head
+    while actualNode.nextNode is not None:
+      actualNode = actualNode.nextNode
+      
+    actualNode.nextNode = newNode
+   
+  # Traversal
+  def traversalList(self):
+    actualNode = self.head
+    while actualNode is not None:
+      print ("%d" % actualNode.data)
+      actualNode = actualNode.nextNode
+     
+# Test 
+linkedlist = LinkedList()
+linkedlist.insertStart(12)
+linkedlist.insertStart(122)
+linkedlist.insertStart(3)
+linkedlist.insertEnd(31)
+
+linkedlist.traversalList()
+print ("Size of linked list is:", linkedlist.size1())
+
+print ("Removing items...")
+linkedlist.remove(31)
+linkedlist.remove(122)
+linkedlist.remove(3)
+linkedlist.remove(12)
+print ("Size of linked list is:", linkedlist.size1())
+
